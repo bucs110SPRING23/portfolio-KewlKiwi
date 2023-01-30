b74c7c868a36d52a5b19678b634410beece528cb
KewlKiwi <123124242+KewlKiwi@users.noreply.github.com>
2023-01-30 17:10:18 -0500

    Update data.py

--- ch01/exercises/data.py
+++ ch01/exercises/data.py
@@ -8,4 +8,4 @@ print(15 % 10)
 print(10 % 15)
 print(10 % 10)
 print(0 % 10)
-print(10 / 15) #yeilds a repeating decimal 6.666... and does not round up or convert to fractional notation
+print(10 / 15) #yields a repeating decimal 6.666... and does not round up or convert to fractional notation
