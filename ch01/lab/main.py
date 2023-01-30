3d0764354ea5f7cd913bf50eb50210744273dc70
Christian Capitano <ccapitano528@gmail.com>
2023-01-30 17:04:25 -0500

    class_exercise

--- ch01/lab/main.py
+++ ch01/lab/main.py
@@ -1,1 +1,33 @@
+import random
 
+#Part A
+weeks = 16
+print(weeks, type(weeks))
+
+classes = 5
+print(classes, type(classes))
+
+tuition = 6000
+print(tuition, type(tuition))
+
+cost_per_week = ((tuition / classes) / weeks)
+print("Cost per week:", cost_per_week)
+
+classes_per_week = 5
+print(classes_per_week, type(classes_per_week))
+
+
+
+#Part B
+
+choice1 = "Cat"
+choice2 = "Dog"
+choice3 = 7
+choice4 = 678
+choice5 = 45.2
+
+list1 = choice1, choice2, choice3, choice4, choice5
+
+randomChoice = random.choice(list1)
+
+print(randomChoice)
