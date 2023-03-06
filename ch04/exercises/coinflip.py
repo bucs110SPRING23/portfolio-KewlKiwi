338dfbc006af2e85081b849de9aa484f95b4c591
Christian Capitano <ccapitano528@gmail.com>
2023-03-06 18:00:29 -0500

    exercise

--- ch04/exercises/coinflip.py
+++ ch04/exercises/coinflip.py
@@ -9,24 +9,35 @@ leonardo = turtle.Turtle()
 leonardo.shape("turtle")
 leonardo.color("green")
 
+done = False
+
 leonardo.goto(0,0)
 
 height = turtle.window_height() / 2
 width = turtle.window_width() / 2
 
-while True:
-    if leonardo.xcor() > width or leonardo.xcor() < -(width) or leonardo.ycor() > height or leonardo.ycor() < -(height):
-        turtle.Screen().bye()
-        break
-    leonardo.forward(50)
-    coinflip = random.randrange(0,2)
-    if coinflip == 0:
+def is_in_screen(window, leonardo):
+   coinflip = random.randrange(0,2)
+   if coinflip == 0:
        coinflip = "Heads"
-    else:
+   else:
        coinflip = "Tails"
 
-    if coinflip == "Heads":
+   if coinflip == "Heads":
        leonardo.left(90)
-    else:
+   else:
        leonardo.right(90)
 
+   leonardo.forward(150)
+
+   if leonardo.xcor() > width or leonardo.xcor() < -(width) or leonardo.ycor() > height or leonardo.ycor() < -(height):
+      turtle.Screen().bye()
+      done = True
+
+while not done:
+    is_in_screen(window, leonardo)
+ #  is_in_screen(window, leonardo)
+  # if done:
+   #    break
+
+is_in_screen(window, leonardo)
