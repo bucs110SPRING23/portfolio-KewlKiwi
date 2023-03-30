d82ff0ffd1a36d2dcf7785320e51d8f673c69ae3
Christian Capitano <ccapitano528@gmail.com>
2023-03-30 00:04:26 -0400

    cipher

--- ch06/exercises/better/substitution.py
+++ ch06/exercises/better/substitution.py
@@ -1,23 +1,41 @@
 import json
 
 
-def main():
-    file_pointer = open("news.txt", "r")
-    var = file_pointer.readlines() 
-    file_pointer2 = open("subs.json", "r")
-    print(file_pointer2)
-    data = json.load(file_pointer2)
-    better = open("betternews.txt", "a")
-    
-    for _ in range(len(var)):
-        for i in range(len(var[_].split())):
-            if (var[_].split())[i] in data.keys():
-                better.write(str(data.keys()))
-            else:
-                better.write((var[_].split())[i] + " ")
-        better.write("\n")
-    file_pointer.close()
-    file_pointer2.close()
-    better.close()
+#def main():
+#    file_pointer = open("news.txt", "r")
+#    var = file_pointer.readlines() 
+#    file_pointer2 = open("subs.json", "r")
+#    print(file_pointer2)
+#    data = json.load(file_pointer2)
+#    better = open("betternews.txt", "a")
+#    
+#    for _ in range(len(var)):
+#        for i in range(len(var[_].split())):
+#            if (var[_].split())[i] in data.keys():
+#                better.write(str(data.keys()))
+#            else:
+#                better.write((var[_].split())[i] + " ")
+#        better.write("\n")
+#    file_pointer.close()
+#    file_pointer2.close()
+#    better.close()
+#
+#main()
 
-main()
+
+
+def main2():
+    text = open("news.txt", "r").read().lower()
+    sub_fptr = open("subs.json", "r")
+    subs = json.load(sub_fptr)
+
+    #use one list to modify another list
+    #which list should you loop through?
+
+    for k, v in subs.items():
+        text = text.replace(k, v)
+    fptr = open("betternews.txt", "w")
+    fptr.write(text)
+    fptr.close()
+
+main2()
