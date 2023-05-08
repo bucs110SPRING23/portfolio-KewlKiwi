6ec461ad9a40dd590c62d9065d394c032bbb1b7f
Christian Capitano <ccapitano528@gmail.com>
2023-05-08 19:07:10 -0400

    Final

--- ch10/final/drink.py
+++ ch10/final/drink.py
@@ -3,9 +3,19 @@ import requests
 class Drink:
 
     def __init__(self):
+        """
+        Initializes the drink object with the url for a random drink
+        args: None
+        return: None
+        """
         self.url = "http://www.thecocktaildb.com/api/json/v1/1/random.php"
 
     def get(self):
+        """
+        Gets data from the url and returns it
+        args: None
+        return: (list) list with keys and values from the url including the name, recipe, and ingredients
+        """
         response2 = requests.get(self.url)
         data = response2.json()
         if data.get('drinks'):
--- ch10/final/food.py
+++ ch10/final/food.py
@@ -3,9 +3,19 @@ import requests
 class Food:
 
     def __init__(self):
+        """
+        Initializes the drink object with the url for a random drink
+        args: None
+        return: None
+        """
         self.url = "https://www.themealdb.com/api/json/v1/1/random.php"
 
     def get(self):
+        """
+        Gets data from the url and returns it
+        args: None
+        return: (list) list with keys and values from the url including the name, recipe, and ingredients
+        """
         response = requests.get(self.url)
         data = response.json()
         if data.get('meals'):
